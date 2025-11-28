42591957eaf76631af54ef8e55ff98bd3a13cc10
Paul Custance <paul.custance@education.gov.uk>
2025-11-28 11:27:42 +0000

    Add assembly scanning for cqrs interface

--- SchoolAccount/SchoolAccount.Application/DependencyInjection.cs
+++ SchoolAccount/SchoolAccount.Application/DependencyInjection.cs
@@ -1,6 +1,23 @@
-﻿namespace SchoolAccount.Application;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SchoolAccount.Application.Abstractions.Messaging;
 
-public class DependencyInjection
+namespace SchoolAccount.Application;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
 }
