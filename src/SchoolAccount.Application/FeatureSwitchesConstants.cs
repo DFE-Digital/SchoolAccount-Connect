e93a30c9bc704c73f2c3d0c0fc25d959e60c1392
ROBERTS, Simon6 <Simon6.ROBERTS@education.gov.uk>
2026-03-11 09:58:28 +0000

    Add `FeatureSwitchesConstants` and refactor feedback feature toggle logic

---
+++ src/SchoolAccount.Application/FeatureSwitchesConstants.cs
@@ -0,0 +1,7 @@
+namespace SchoolAccount.Application;
+
+public static class FeatureSwitchesConstants
+{
+    public const string CalendarOfItems = nameof(CalendarOfItems);
+    public const string ConnectFeedback = nameof(ConnectFeedback);
+}
