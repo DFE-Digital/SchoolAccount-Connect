9fff82d814dd1d15c7293d1ce107b298d0a2adb0
ROBERTS, Simon6 <Simon6.ROBERTS@education.gov.uk>
2026-03-11 13:47:22 +0000

    Remove `FeatureSwitchesConstants` and update references to `FeatureNameConstants` for feedback feature toggle

--- src/SchoolAccount.Application/FeatureSwitchesConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace SchoolAccount.Application;
-
-public static class FeatureSwitchesConstants
-{
-    public const string CalendarOfItems = nameof(CalendarOfItems);
-    public const string ConnectFeedback = nameof(ConnectFeedback);
-}
--- src/SchoolAccount.Web.Connect/FeatureNameConstants.cs
+++ src/SchoolAccount.Web.Connect/FeatureNameConstants.cs
@@ -4,4 +4,5 @@ public static class FeatureNameConstants
 {
     public const string MaintenanceMode = "MaintenanceMode";
     public const string ButtonTesting = "ButtonTesting";
+    public const string ConnectFeedback = "ConnectFeedback";
 }
