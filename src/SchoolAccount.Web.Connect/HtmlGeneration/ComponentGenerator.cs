c57cb38f49c2c1166dabd9b857e9b5dc9e6abcb8
Kian Winstanley <kian.winstanley@education.gov.uk>
2026-03-26 16:11:29 +0000

    Remove unused code

--- src/SchoolAccount.Web.Connect/HtmlGeneration/ComponentGenerator.cs
+++ src/SchoolAccount.Web.Connect/HtmlGeneration/ComponentGenerator.cs
@@ -1,79 +1,54 @@
-﻿using System.Text.Encodings.Web;
-using GovUk.Frontend.AspNetCore;
-using Microsoft.AspNetCore.Html;
-using SoftCircuits.HtmlMonkey;
-using ReadOnlyTagHelperAttributeList = Microsoft.AspNetCore.Razor.TagHelpers.ReadOnlyTagHelperAttributeList;
-using TagHelperAttribute = Microsoft.AspNetCore.Razor.TagHelpers.TagHelperAttribute;
-using TagHelperAttributeList = Microsoft.AspNetCore.Razor.TagHelpers.TagHelperAttributeList;
-using TagHelperOutput = Microsoft.AspNetCore.Razor.TagHelpers.TagHelperOutput;
+﻿using GovUk.Frontend.AspNetCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SchoolAccount.Web.Connect.HtmlGeneration;
 
-internal static partial class ComponentGenerator
+internal partial class ComponentGenerator
 {
-    internal static void ApplyComponentHtml(
-        this TagHelperOutput output,
-        IHtmlContent content,
-        HtmlEncoder? encoder = null
+    internal static TagBuilder Generate(
+        string nodeName,
+        string? className = null,
+        string? text = null,
+        bool treatAsHtml = false,
+        params (string, string?)[] attributes
     )
     {
-        ArgumentNullException.ThrowIfNull(output);
-        ArgumentNullException.ThrowIfNull(content);
+        var node = new TagBuilder(nodeName);
 
-        encoder ??= HtmlEncoder.Default;
-        ArgumentNullException.ThrowIfNull(encoder);
-
-        var unwrapped = UnwrapComponent(content, encoder);
-        ArgumentNullException.ThrowIfNull(unwrapped);
-
-        output.TagName = unwrapped.TagName;
-        output.TagMode = unwrapped.TagMode;
-
-        output.Attributes.Clear();
-
-        foreach (var attribute in unwrapped.Attributes)
+        if (!string.IsNullOrEmpty(className))
         {
-            output.Attributes.Add(attribute);
+            node.MergeCssClass(className);
         }
 
-        output.Content.AppendHtml(unwrapped.InnerHtml);
-    }
-
-    internal static ComponentTagHelperOutput? UnwrapComponent(IHtmlContent content, HtmlEncoder encoder)
-    {
-        return UnwrapComponent(content.ToHtmlString(encoder));
-    }
-
-    internal static ComponentTagHelperOutput? UnwrapComponent(string html)
-    {
-        if (string.IsNullOrWhiteSpace(html))
+        if (!string.IsNullOrEmpty(text))
         {
-            return null;
+            if (treatAsHtml)
+            {
+                node.InnerHtml.AppendHtml(text);
+            }
+            else
+            {
+                node.InnerHtml.Append(text);
+            }
         }
 
-        var doc = HtmlDocument.FromHtml(html);
-        var root = (HtmlElementNode)doc.RootNodes.Single(n => n is HtmlElementNode);
+        if (attributes.Length > 0)
+        {
+            return node;
+        }
 
-        var tagName = root.TagName;
-        var tagMode = root.IsSelfClosing
-            ? Microsoft.AspNetCore.Razor.TagHelpers.TagMode.SelfClosing
-            : Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag;
-        var attributes = new TagHelperAttributeList(
-            root.Attributes.Select(a =>
-                a.Value is null
-                    ? new TagHelperAttribute(a.Name)
-                    : new TagHelperAttribute(a.Name, new HtmlString(a.Value))
-            )
-        );
-        var innerHtml = new HtmlString(root.InnerHtml);
+        foreach (var (key, property) in attributes)
+        {
+            if (property == null)
+            {
+                node.Attributes.Remove(key);
+            }
+            else
+            {
+                node.MergeAttribute(key, property);
+            }
+        }
 
-        return new ComponentTagHelperOutput(tagName, tagMode, attributes, innerHtml);
+        return node;
     }
-
-    internal sealed record ComponentTagHelperOutput(
-        string? TagName,
-        Microsoft.AspNetCore.Razor.TagHelpers.TagMode TagMode,
-        ReadOnlyTagHelperAttributeList Attributes,
-        IHtmlContent InnerHtml
-    );
 }
