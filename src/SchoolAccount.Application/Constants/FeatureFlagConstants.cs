24e8b099ae0073e9f7394cb2b5d639ffcd79b352
ROBERTS, Simon6 <Simon6.ROBERTS@education.gov.uk>
2026-04-17 08:59:22 +0000

    Remove "ConnectFeedback" feature flag and rename _FeedbackV2 to _Feedback
    Remove "ConnectFeedback" feature flag and rename _FeedbackV2 to _Feedback
    
    Related work items: #262582

--- src/SchoolAccount.Application/Constants/FeatureFlagConstants.cs
+++ src/SchoolAccount.Application/Constants/FeatureFlagConstants.cs
@@ -3,7 +3,5 @@ namespace SchoolAccount.Application.Constants;
 public static class FeatureFlagConstants
 {
     public const string MaintenanceMode = "MaintenanceMode";
-    public const string ButtonTesting = "ButtonTesting";
-    public const string ConnectFeedback = "ConnectFeedback";
     public const string FeedbackBanner = "FeedbackBanner";
 }
