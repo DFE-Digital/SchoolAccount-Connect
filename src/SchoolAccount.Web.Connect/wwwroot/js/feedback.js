3f1f5a095d10574f19efc44bfe2989bda2fad39a
ROBERTS, Simon6 <Simon6.ROBERTS@education.gov.uk>
2026-04-21 09:55:06 +0000

    refactor js for hiding the banner
    Related work items: #263946

--- src/SchoolAccount.Web.Connect/wwwroot/js/feedback.js
+++ src/SchoolAccount.Web.Connect/wwwroot/js/feedback.js
@@ -1,5 +1,13 @@
 (function () {
-    var dismissedKey = "feedback-banner-dismissed";
+    const connectBannerDismissedKey = "connect_banner_hidden";
+
+    function isBannerDismissed() {
+        return sessionStorage.getItem(connectBannerDismissedKey) === "true";
+    }
+
+    function dismissBanner() {
+        sessionStorage.setItem(connectBannerDismissedKey, "true");
+    }
 
     function postFeedback(postUrl, payload) {
         fetch(postUrl, {
@@ -9,18 +17,81 @@
         }).catch(function () { });
     }
 
-    function isBannerDismissed() {
-        try {
-            return sessionStorage.getItem(dismissedKey) === "true";
-        } catch (e) {
-            return false;
-        }
-    }
+    function initialisePageUsefulFeedback(root) {
+        var initial = root.querySelector("[data-feedback-initial]");
+        var followup = root.querySelector("[data-feedback-followup]");
+        var buttons = root.querySelectorAll("[data-feedback-answer]");
+        var cancelButton = root.querySelector("[data-feedback-cancel]");
+        var feedbackLink = root.querySelector("[data-feedback-exit-link]");
 
-    function dismissBanner() {
-        try {
-            sessionStorage.setItem(dismissedKey, "true");
-        } catch (e) { }
+        var postUrl = root.getAttribute("data-feedback-post-url") || "/feedback/page-useful";
+        var pageId = root.getAttribute("data-page-id");
+        var ctaType = root.getAttribute("data-cta-type");
+        var selectedAnswer = null;
+
+        function showFollowup() {
+            if (!initial || !followup) {
+                return;
+            }
+
+            initial.classList.add("govuk-!-display-none");
+            followup.classList.remove("govuk-!-display-none");
+        }
+
+        function showInitial() {
+            if (!initial || !followup) {
+                return;
+            }
+
+            followup.classList.add("govuk-!-display-none");
+            initial.classList.remove("govuk-!-display-none");
+
+            buttons.forEach(function (button) {
+                button.classList.remove("dfe-page-feedback__choice--selected");
+            });
+
+            selectedAnswer = null;
+        }
+
+        buttons.forEach(function (button) {
+            button.addEventListener("click", function () {
+                selectedAnswer = this.getAttribute("data-feedback-answer");
+
+                buttons.forEach(function (b) {
+                    b.classList.remove("dfe-page-feedback__choice--selected");
+                });
+
+                this.classList.add("dfe-page-feedback__choice--selected");
+
+                postFeedback(postUrl, {
+                    eventName: "connect_cta_yes_no_interaction",
+                    pageId: pageId,
+                    ctaType: ctaType,
+                    selectedAnswer: selectedAnswer
+                });
+
+                showFollowup();
+            });
+        });
+
+        if (cancelButton) {
+            cancelButton.addEventListener("click", function () {
+                postFeedback(postUrl, {
+                    eventName: "connect_cta_cancelled",
+                    pageId: pageId,
+                    ctaType: ctaType,
+                    selectedAnswer: selectedAnswer
+                });
+
+                showInitial();
+            });
+        }
+
+        if (feedbackLink) {
+            feedbackLink.addEventListener("click", function () {
+                showInitial();
+            });
+        }
     }
 
     function initialisePageUsefulFeedback(root) {
@@ -128,6 +199,7 @@
 
         if (feedbackLink) {
             feedbackLink.addEventListener("click", function () {
+                dismissBanner();
                 root.classList.add("govuk-!-display-none");
             });
         }
