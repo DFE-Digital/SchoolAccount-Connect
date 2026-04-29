579bc49b2038af73a0abed920decc111fb3b92cb
ROBERTS, Simon6 <Simon6.ROBERTS@education.gov.uk>
2026-04-29 12:54:52 +0000

    Simplify feedback interaction logic and update telemetry events
    Simplify feedback interaction logic and update telemetry events
    
    - Remove unnecessary duplicate logic from `feedback.js`.
    - Update `connect_cta_*` telemetry events and ensure `selectedAnswer` is set to `null` where appropriate.
    
    Related work items: #262582
    
    Related work items: #262582

--- src/SchoolAccount.Web.Connect/wwwroot/js/feedback.js
+++ src/SchoolAccount.Web.Connect/wwwroot/js/feedback.js
@@ -80,7 +80,7 @@
                     eventName: "connect_cta_cancelled",
                     pageId: pageId,
                     ctaType: ctaType,
-                    selectedAnswer: selectedAnswer
+                    selectedAnswer: null
                 });
 
                 showInitial();
@@ -89,83 +89,13 @@
 
         if (feedbackLink) {
             feedbackLink.addEventListener("click", function () {
-                showInitial();
-            });
-        }
-    }
-
-    function initialisePageUsefulFeedback(root) {
-        var initial = root.querySelector("[data-feedback-initial]");
-        var followup = root.querySelector("[data-feedback-followup]");
-        var buttons = root.querySelectorAll("[data-feedback-answer]");
-        var cancelButton = root.querySelector("[data-feedback-cancel]");
-        var feedbackLink = root.querySelector("[data-feedback-exit-link]");
-
-        var postUrl = root.getAttribute("data-feedback-post-url") || "/feedback/page-useful";
-        var pageId = root.getAttribute("data-page-id");
-        var ctaType = root.getAttribute("data-cta-type");
-        var selectedAnswer = null;
-
-        function showFollowup() {
-            if (!initial || !followup) {
-                return;
-            }
-
-            initial.classList.add("govuk-!-display-none");
-            followup.classList.remove("govuk-!-display-none");
-        }
-
-        function showInitial() {
-            if (!initial || !followup) {
-                return;
-            }
-
-            followup.classList.add("govuk-!-display-none");
-            initial.classList.remove("govuk-!-display-none");
-
-            buttons.forEach(function (button) {
-                button.classList.remove("dfe-page-feedback__choice--selected");
-            });
-
-            selectedAnswer = null;
-        }
-
-        buttons.forEach(function (button) {
-            button.addEventListener("click", function () {
-                selectedAnswer = this.getAttribute("data-feedback-answer");
-
-                buttons.forEach(function (b) {
-                    b.classList.remove("dfe-page-feedback__choice--selected");
-                });
-
-                this.classList.add("dfe-page-feedback__choice--selected");
-
                 postFeedback(postUrl, {
-                    eventName: "connect_cta_yes_no_interaction",
+                    eventName: "connect_cta_feedback_exit",
                     pageId: pageId,
                     ctaType: ctaType,
-                    selectedAnswer: selectedAnswer
+                    selectedAnswer: null
                 });
 
-                showFollowup();
-            });
-        });
-
-        if (cancelButton) {
-            cancelButton.addEventListener("click", function () {
-                postFeedback(postUrl, {
-                    eventName: "connect_cta_cancelled",
-                    pageId: pageId,
-                    ctaType: ctaType,
-                    selectedAnswer: selectedAnswer
-                });
-
-                showInitial();
-            });
-        }
-
-        if (feedbackLink) {
-            feedbackLink.addEventListener("click", function () {
                 showInitial();
             });
         }
@@ -199,6 +129,13 @@
 
         if (feedbackLink) {
             feedbackLink.addEventListener("click", function () {
+                postFeedback(postUrl, {
+                    eventName: "connect_cta_feedback_exit",
+                    pageId: pageId,
+                    ctaType: ctaType,
+                    selectedAnswer: null
+                });
+
                 dismissBanner();
                 root.classList.add("govuk-!-display-none");
             });
