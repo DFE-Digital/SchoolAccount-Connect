a2971c6275bca76a42ab544fbecd30cfccd4f4cc
KELLY, Chris <Chris.KELLY@education.gov.uk>
2026-05-29 08:57:18 +0000

    Add tag helper to making a link open in a new tab simple
    Add tag helper to making a link open in a new tab simple
    
    Related work items: #277376

---
+++ src/SchoolAccount.Web.Connect/Helpers/OpenNewTabTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SchoolAccount.Web.Connect.Helpers;
+
+[HtmlTargetElement("a", Attributes = OpenNewTabTagName)]
+public class OpenNewTabTagHelper : TagHelper
+{
+    private const string OpenNewTabTagName = "open-new-tab";
+
+    public override int Order => int.MinValue;
+
+    [HtmlAttributeName(OpenNewTabTagName)]
+    public bool? OpenInNewTab { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll(OpenNewTabTagName);
+        
+        if (OpenInNewTab.HasValue && OpenInNewTab.Value)
+        {
+            output.Attributes.Add("target", "_blank");
+            output.Attributes.Add("rel", "noreferrer noopener");
+            
+            var content = await output.GetChildContentAsync();
+            output.Content.SetHtmlContent(
+                $"{content.GetContent()} (opens in new tab)"
+            );
+
+        }
+    }
+}
