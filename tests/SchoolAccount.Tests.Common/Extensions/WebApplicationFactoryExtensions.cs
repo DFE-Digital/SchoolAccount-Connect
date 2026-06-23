0a03acfc49b7d1a62337ac698b577fa5705e3655
Paul Custance <paul.custance@education.gov.uk>
2026-06-24 00:37:16 +0100

    Improve error output when calls fail

--- tests/SchoolAccount.Tests.Common/Extensions/WebApplicationFactoryExtensions.cs
+++ tests/SchoolAccount.Tests.Common/Extensions/WebApplicationFactoryExtensions.cs
@@ -1,4 +1,6 @@
+using AngleSharp;
 using AngleSharp.Dom;
+using AngleSharp.Html;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace SchoolAccount.Tests.Common.Extensions;
@@ -19,8 +21,11 @@ public static class WebApplicationFactoryExtensions
         if (!response.IsSuccessStatusCode)
         {
             var errorHtml = await response.Content.ReadAsStringAsync();
+            var context = BrowsingContext.New(Configuration.Default);
+            var doc = await context.OpenAsync(req => req.Content(errorHtml));
+            var formatted = doc.ToHtml(new PrettyMarkupFormatter());
             throw new HttpRequestException(
-                $"Request to {uri} failed with {response.StatusCode}.\nResponse Content:\n{errorHtml}"
+                $"Request to {uri} failed with {response.StatusCode}.\nResponse Content:\n{formatted}"
             );
         }
 
